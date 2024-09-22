180907206dcaa8da7aaab75829f50bdb29a7cf1b
“Oleg <o.alekhnovich@gmail.com>
2024-09-23 00:56:20 +0300

    update: installed swagger ui

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(base_path('routes/api_v1.php'));
+Route::prefix('v2')->group(base_path('routes/api_v2.php'));
